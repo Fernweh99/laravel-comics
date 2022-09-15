f9272da51ce33e9eb077ed818f99c2224e7ac5dd
Fernweh99 <daniele.dimento@gmail.com>
2022-09-15 19:02:55 +0200

    add footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,75 @@
+<footer>
+
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-2 py-3">
+          <h4>DC COMICS</h4>
+          <ul>
+            <li><a href="#">Character</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+          <h4>SHOP</h4>
+          <ul>
+            <li><a href="#">Shop Dc</a></li>
+            <li><a href="#">Shop Dc Collectibles</a></li>
+          </ul>
+        </div>
+        
+        <div class="col-2 py-3">
+          <h4>DC</h4>
+          <ul>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy policy (new)</a></li>
+            <li><a href="#">ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+        
+        <div class="col-2 py-3">
+          <h4>SITES</h4>
+          <ul>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-img">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <nav class="container">
+        <div class="row">
+          <div class="col">
+            <a class="btn" href="#">SIGN-UP NOW!</a>
+          </div>
+          <div class="col d-flex align-items-center justify-content-end">
+            <h4>FOLLOW US</h4>
+            <ul>
+              <li><a href="#"><img src="{{asset('images/footer-facebook.png')}}" alt="facebook icon" /></a></li>
+              <li><a href="#"><img src="{{asset('images/footer-twitter.png')}}" alt="twitter icon" /></a></li>
+              <li><a href="#"><img src="{{asset('images/footer-youtube.png')}}" alt="youtube icon" /></a></li>
+              <li><a href="#"><img src="{{asset('images/footer-pinterest.png')}}" alt="pinterest icon" /></a></li>
+              <li><a href="#"><img src="{{asset('images/footer-periscope.png')}}" alt="periscope icon" /></a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </footer>
